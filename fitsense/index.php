4040b979df4c4099dba47a36f29f25fe3c9967f5
Gal Sarig <gal2016@latingate.com>
2023-11-27 11:24:38 +0200

    =s

---
+++ fitsense/index.php
@@ -0,0 +1,9 @@
+<br/><br/><br/><br/><br/>
+<div style="text-align: center; color: purple">
+    <h1>
+        FitSense.co.il
+    </h1>
+    <h2>
+        Fitness that makes sense
+    </h2>
+</div>
